346044b195fc2d4aadac5570320dffdcd7475e74
Deepak Pandey <75252930+Dee-Pandey@users.noreply.github.com>
2020-12-06 15:50:19 +0530

    Update MyTicTacToe.java

--- MyTicTacToe.java
+++ MyTicTacToe.java
@@ -123,24 +123,24 @@ public class MyTicTacToeGame {
 	
 	public static String computeWinner()
 	{
-		List topRow = Arrays.asList(1,2,3);
-		List midRow = Arrays.asList(4,5,6);
-		List bottomRow = Arrays.asList(7,8,9);
-		List leftCol = Arrays.asList(1,4,7);
-		List midCol = Arrays.asList(2,5,8);
-		List rightCol = Arrays.asList(3,6,9);
-		List cross1 = Arrays.asList(1,5,9);
-		List cross2 = Arrays.asList(7,5,3);
+		List<Integer> firstRow = Arrays.asList(1,2,3);
+		List<Integer> secondRow = Arrays.asList(4,5,6);
+		List<Integer> thirdRow = Arrays.asList(7,8,9);
+		List<Integer> firstColumn = Arrays.asList(1,4,7);
+		List<Integer> secondColumn = Arrays.asList(2,5,8);
+		List<Integer> thirdColumn = Arrays.asList(3,6,9);
+		List<Integer> diagonal1 = Arrays.asList(1,5,9);
+		List<Integer> diagonal2 = Arrays.asList(7,5,3);
 		
 		List<List> winning = new ArrayList<List>();
-		winning.add(topRow);
-		winning.add(midRow);
-		winning.add(bottomRow);
-		winning.add(leftCol);
-		winning.add(midCol);
-		winning.add(rightCol);
-		winning.add(cross1);
-		winning.add(cross2);
+		winning.add(firstRow);
+		winning.add(secondRow);
+		winning.add(thirdRow);
+		winning.add(firstColumn);
+		winning.add(secondColumn);
+		winning.add(thirdColumn);
+		winning.add(diagonal1);
+		winning.add(diagonal2);
 		
 		for(List l : winning) {
 			if(playerXPositions.containsAll(l)) 
